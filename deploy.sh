6779c0911dbd7603347d3e2d128e87e5198197c2
Rachel <rachelgoree26@gmail.com>
2020-04-20 17:19:32 +0200

    heroku pages

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<bowlofsunshine>/<bowlofsunshine>.github.io.git master
+# # if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<bowlofsunshine>/<bowlofsunshine>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:bowlofsunshine/todo.git master:gh-pages
